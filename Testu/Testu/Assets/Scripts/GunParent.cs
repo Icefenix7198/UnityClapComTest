07ef89c956f8526122b4cc5e7227824ed0efe83a
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-03-13 11:29:15 +0100

    Get listado enemigos y mirarles
    Mira hacia el enemigo mas cercano peeeeeeeeeeeeeeeeero aun no dispara :(
    
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- Testu/Testu/Assets/Scripts/GunParent.cs
+++ Testu/Testu/Assets/Scripts/GunParent.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GunParent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Stats weapon
     public float damage;
     public float cadency;
     public float distanceBullet;
     public float speedBullet;
+
+    //Referencias
     public GameObject projectile;
     public Transform parent;
+    public EnemiesDetection detection;
+    public List<GameObject> enemigos;
+    [SerializeField] private GameObject enemyToShoot;
+    float distanceEnemy = float.PositiveInfinity;
 
-    //Tenemos un tiempo solo para la espera entre disparos y un tiempo para la actualizacion de las balas (?)
+    //Time para disparar
     float shootingTime = 0.0f;
 
     // Update is called once per frame
@@ -21,11 +28,29 @@ public class GunParent : MonoBehaviour
         shootingTime += Time.deltaTime;
         if (Input.GetKey(KeyCode.J)) 
         {
+            enemigos = detection.GetComponent<EnemiesDetection>().listEnemies;
+            //Orientarse hacia el enememigo
+            for (int i = 0; i < enemigos.Count; i++) 
+            {
+                if (Vector3.Distance(enemigos[i].transform.position,parent.position) < distanceEnemy) 
+                {
+                    enemyToShoot = enemigos[i];
+                    distanceEnemy = Vector3.Distance(enemigos[i].transform.position, parent.position);
+                }
+            }
+
+            //Set rotacion del disparo
+            if(enemigos != null) 
+            {
+                //Direccion del enemigo
+                //Vector3 dir = enemyToShoot.transform.position - parent.position;
+                transform.LookAt(enemyToShoot.transform);
+            }
+
             if (shootingTime > cadency) 
             {
                 //Create bullet and set its characteristics
-                
-                GameObject referencia = Instantiate(projectile,parent.position, Quaternion.identity);
+                GameObject referencia = Instantiate(projectile,parent.position, transform.rotation);
                 referencia.GetComponent<BulletMovement>().maxDistance = distanceBullet;
                 referencia.GetComponent<BulletMovement>().speed = speedBullet;
                 referencia.GetComponent<BulletMovement>().damage = damage;
