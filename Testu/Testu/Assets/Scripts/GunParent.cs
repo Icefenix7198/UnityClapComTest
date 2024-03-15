3e36b8b30fd0b90fa15906514b29b2e2eb24eee4
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-03-15 10:20:44 +0100

    Gun Parent Recarga

--- Testu/Testu/Assets/Scripts/GunParent.cs
+++ Testu/Testu/Assets/Scripts/GunParent.cs
@@ -5,11 +5,31 @@ using static UnityEngine.GraphicsBuffer;
 
 public class GunParent : MonoBehaviour
 {
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class GunParent : MonoBehaviour
+{
     // Stats weapon
+    public GameObject actualWeapon; //se le puede poner un prefab de arma en vez de assignar a mano los valores 
+
     public float damage;
     public float cadency;
     public float distanceBullet;
     public float speedBullet;
+    public bool piercing;
+    public int maxMagazine;
+    public float reloadTime;
+    int currentBullets;
+    enum shotType
+    {
+        NORMAL,
+        SHOTGUN1,
+        SHOTGUN2,
+    }
+    [SerializeField] private shotType tipoDisparo;
 
     //Referencias
     public GameObject projectile;
@@ -22,46 +42,74 @@ public class GunParent : MonoBehaviour
     //Time para disparar
     float shootingTime = 0.0f;
 
+    private void Start()
+    {
+        if (actualWeapon != null && actualWeapon.GetComponent<WeaponInfo>())
+        {
+            damage = actualWeapon.GetComponent<WeaponInfo>().damage;
+            cadency = actualWeapon.GetComponent<WeaponInfo>().cadency;
+            distanceBullet = actualWeapon.GetComponent<WeaponInfo>().distanceBullet;
+            speedBullet = actualWeapon.GetComponent<WeaponInfo>().speedBullet;
+            piercing = actualWeapon.GetComponent<WeaponInfo>().piercing;
+            maxMagazine = actualWeapon.GetComponent<WeaponInfo>().maxMagazine;
+            reloadTime = actualWeapon.GetComponent<WeaponInfo>().reloadTime;
+
+        }
+
+        currentBullets = maxMagazine;
+    }
+
     // Update is called once per frame
     void Update()
     {
         shootingTime += Time.deltaTime;
-        if (Input.GetKey(KeyCode.J)) 
+        if (currentBullets <= 0)
         {
-            enemigos = detection.GetComponent<EnemiesDetection>().listEnemies;
-            //Orientarse hacia el enememigo
-            for (int i = 0; i < enemigos.Count; i++) 
+            if (shootingTime > reloadTime) { currentBullets = maxMagazine; }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.J))
             {
-                if (Vector3.Distance(enemigos[i].transform.position,parent.position) < distanceEnemy) 
+                enemigos = detection.GetComponent<EnemiesDetection>().listEnemies;
+                //Orientarse hacia el enememigo
+                for (int i = 0; i < enemigos.Count; i++)
                 {
-                    enemyToShoot = enemigos[i];
-                    distanceEnemy = Vector3.Distance(enemigos[i].transform.position, parent.position);
+                    if (Vector3.Distance(enemigos[i].transform.position, parent.position) < distanceEnemy)
+                    {
+                        enemyToShoot = enemigos[i];
+                        distanceEnemy = Vector3.Distance(enemigos[i].transform.position, parent.position);
+                    }
+                }
+
+                //Set rotacion del disparo
+                Vector3 shotDir = transform.forward;
+                if (enemigos != null)
+                {
+                    //Direccion del enemigo
+                    shotDir = enemyToShoot.transform.position - parent.position;
+                    //transform.LookAt(enemyToShoot.transform);
+                }
+
+                if (shootingTime > cadency)
+                {
+                    //Create bullet and set its characteristics
+                    GameObject referencia = Instantiate(projectile, parent.position, Quaternion.identity);
+                    referencia.GetComponent<BulletMovement>().maxDistance = distanceBullet;
+                    referencia.GetComponent<BulletMovement>().speed = speedBullet;
+                    referencia.GetComponent<BulletMovement>().damage = damage;
+                    referencia.GetComponent<BulletMovement>().direction = shotDir;
+
+                    //Deploy bullets
+                    currentBullets -= 1; //Lo pongo por si hay en un futuro gasto de multiples balas como en la escopeta, sino simplemente poner la otra formula que sera mas eficiente
+                    //currentBullets--;
+
+                    //Reset time to shoot
+                    shootingTime = 0.0f;
                 }
             }
-
-            //Set rotacion del disparo
-            Vector3 shotDir = transform.forward;
-            if(enemigos != null) 
-            {
-                //Direccion del enemigo
-                shotDir = enemyToShoot.transform.position - parent.position;
-                //transform.LookAt(enemyToShoot.transform);
-            }
-
-            if (shootingTime > cadency) 
-            {
-                //Create bullet and set its characteristics
-                GameObject referencia = Instantiate(projectile,parent.position, Quaternion.identity);
-                referencia.GetComponent<BulletMovement>().maxDistance = distanceBullet;
-                referencia.GetComponent<BulletMovement>().speed = speedBullet;
-                referencia.GetComponent<BulletMovement>().damage = damage;
-                referencia.GetComponent<BulletMovement>().direction = shotDir;
-
-                //More things would go here
-
-                //Reset time to shoot
-                shootingTime = 0.0f;
-            }
         }
+
     }
+}
 }
